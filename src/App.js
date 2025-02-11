21ccfe4125138fca8d01d0fd95c0ad47cfc09107
TanayaKarande <tanaya.karande@anveshak.com>
2025-02-11 18:37:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react from "react";
+import { Navbar } from "./Components/Navbar";
+import { Header } from "./Components/Header";
+import { Feature } from "./Components/Feature";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Feature />
+    </div>
+  );
+}
+
+export default App;
